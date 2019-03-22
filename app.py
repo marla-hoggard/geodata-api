70c218525c057eaa51f8bfa98cbe7c1820a13dc7
Marla Glasser <mbglasser@gmail.com>
2019-03-21 23:49:39 -0400

    Flask API. Reads from csv and filters on params.

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+import pandas as pd
+    
+app = Flask(__name__)
+
+'''
+Returns [{ network: <IP>, latitude: #, longitude: # }] 
+for all IP addresses within a bounding box specified by query params:
+@lat: center latitude
+@lg: center longitude
+@ht: height (in latitude) of bounding box
+@width: width (in longitude) of bounding box
+Example query: http:/.../?lat=30&lg=20&ht=40&width=30`
+'''
+@app.route('/')
+def get_networks_in_range():
+  lat = float(request.args.get('lat'))
+  lg = float(request.args.get('lg'))
+  height = float(request.args.get('ht'))
+  width = float(request.args.get('width'))
+
+  df = pd.read_csv('geolite2-city-blocks-IPv4.csv', usecols=['network', 'latitude', 'longitude'])
+  df = df[(df['latitude'] >= lat - height/2) & (df['latitude'] <= lat + height/2)]
+  df = df[(df['longitude'] >= lg - width/2) & (df['longitude'] <= lg + width/2)]
+  return df.to_json(orient='records')
+
+if __name__ == '__main__':
+  app.run(debug=True)
